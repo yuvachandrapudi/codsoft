6f0e1489a18859154b681a1425210700fa0dd307
yuvachandrapudi <164348605+yuvachandrapudi@users.noreply.github.com>
2024-03-29 11:30:22 +0530

    Create student_grade_calculator.java
    A short JAVA program to calculate the grade of the students

---
+++ student_grade_calculator.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class STUDENT_GRADE_CALCULATOR {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+            String[] Subject = {"DBMS", "JAVA", "PS", "DAA", "MEFA"};
+            int[] Marks = new int[Subject.length];
+        for (int i = 0; i < Subject.length; i++) {
+            int mark;
+            do {
+                System.out.print("Enter the " + Subject[i] + " Marks : ");
+                mark = sc.nextInt();
+                if (mark > 100) {
+                    System.out.println("Enter a valid marks (0-100).");
+                }
+            } while (mark > 100);
+
+            Marks[i] = mark;
+        }
+            double avg = Arrays.stream(Marks).average().getAsDouble();
+            System.out.println("Sum of Marks : " + Arrays.stream(Marks).sum());
+            System.out.printf("Average Marks : %.2f%n", avg);
+            if (avg >= 91 && avg <= 100) {
+                System.out.println("Grade A");
+            } else if (avg >= 81 && avg < 91) {
+                System.out.println("Grade B");
+            } else if (avg >= 71 && avg < 81) {
+                System.out.println("Grade C");
+            } else if (avg >= 61 && avg < 71) {
+                System.out.println("Grade D");
+            } else if (avg >= 51 && avg < 61) {
+                System.out.println("Grade E");
+            } else {
+                System.out.println("Failed");
+            }
+        }
+    }
